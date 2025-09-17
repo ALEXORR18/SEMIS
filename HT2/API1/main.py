d9ab2032d8052c0dd5bf92a896a5f1d423e9a37d
ALEXORR18 <brian7orr@gmail.com>
2025-09-16 18:13:21 -0600

    feat: Api's y dependencias

---
+++ HT2/API1/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+# Endpoint 1: /health
+@app.get("/health")
+def health_check():
+    # Retorna solo el código 200 con un mensaje simple
+    return JSONResponse(content={"status": "ok"}, status_code=200)
+
+# Endpoint 2: /get-data
+@app.get("/get-data")
+def get_data():
+    return {
+        "Instancia": "Maquina 1 - API 1",
+        "Curso": "Seminario de sistemas 1 A",
+        "Grupo": "Grupo 11"
+    }
